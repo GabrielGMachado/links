c6bd608e279196f79f68c04db257981d906d9db3
GabrielGMachado <bielgaelzermachado@gmail.com>
2026-02-25 21:54:35 -0300

    test commit

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,11 @@
 import { Stack } from "expo-router"; // navegação em pilha, uma tela vai abrir sobre a outra, deixa mais rápido as coisas, mas provavelmente mais pesado caso seja mt grande o app
 
+import { colors } from "@/styles/colors";
+
 export default function Layout() {
-  return <Stack screenOptions={{
-    headerShown: false,
+  return <Stack 
+    screenOptions={{
+      headerShown: false,
+      contentStyle: { backgroundColor: colors.gray[950]}
   }}/>
 }
