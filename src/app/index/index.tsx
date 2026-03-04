eebe1b40b7f0089e732221148a0534b4bfee531f
GabrielGMachado <bielgaelzermachado@gmail.com>
2026-03-04 20:47:50 -0300

    feat: categories with flatlist component

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -3,8 +3,7 @@ import { MaterialIcons } from "@expo/vector-icons"
 
 import { styles } from "./styles"
 import { colors } from "@/styles/colors"
-
-import { Category } from "@/components/category"
+import { Categories } from "@/components/categories"
 
 export default function Index() {
   return (
@@ -16,9 +15,8 @@ export default function Index() {
           <MaterialIcons name="add" size={ 32 } color={ colors.green[300] }/>
         </TouchableOpacity>
       </View>
-      <Category name="Projeto" icon="code" isSelected/>
-      <Category name="Site" icon="language" isSelected={false}/>
-      <Category name="Vídeo" icon="movie" isSelected={false}/>
+
+      <Categories/>      
     </View>
   )
 }
